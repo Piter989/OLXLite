85ab79f974a69fb6f5824afa319e6fbd3f4855c7
Piotr <piotr.smma@gmail.com>
2025-05-15 21:16:22 +0200

    git init

---
+++ olxLite-bff/src/main/java/com/olxLite/olxLite_bff/OlxLiteBffApplication.java
@@ -0,0 +1,13 @@
+package com.olxLite.olxLite_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxLiteBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxLiteBffApplication.class, args);
+	}
+
+}
---
+++ olxLite-bff/src/test/java/com/olxLite/olxLite_bff/OlxLiteBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olxLite.olxLite_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlxLiteBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
