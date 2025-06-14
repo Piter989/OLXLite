1a3875d0b5d79a9451009bca501469b8d7fb7531
Piotr <piotr.smma@gmial.com>
2025-06-14 23:40:24 +0200

    Add fronted and simple test

---
+++ olxlite-app/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+fetch('item')
+  .then(res => res.json())
+  .then(data => {
+    console.log('Backend response:', data);
+  })
+  .catch(err => {
+    console.error('Backend connection error:', err);
+  });
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
